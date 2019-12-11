69afd702c0ea54ea0b25cd0673774c137b1e5cc4
vinodh1988 <k.c.vinodh@hotmail.com>
2019-12-11 14:38:42 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import  {MessageRedux,ProgrammerRedux,PeopleRedux} from './Redux/mappings/mapping'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const App=()=>{
+  return(
+      <div id="home">
+            <PeopleRedux/>
+            <MessageRedux/>
+            <ProgrammerRedux/>
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {state}from './Redux/mappings/mapping';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore,applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {peopleRead} from './Redux/actions/peopleAction';
+
+import {Provider} from 'react-redux';
+
+const store=createStore(state,applyMiddleware(thunk));
+store.dispatch(peopleRead());
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
